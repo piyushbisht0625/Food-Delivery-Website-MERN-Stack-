f389c6f0e8d2c4168dc16c7e1d2eb59457cc20a5
Piyush Bisht <piyushbisht0625@gmail.com>
2024-04-04 10:25:34 +0530

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom"; 
 
-//import Badge from "@material-ui/core/Badge";
+//import Badge from "@material-ui/core/Badge"; 
 //import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { useCart } from './ContextReducer';
 import Modal from '../Modal';
